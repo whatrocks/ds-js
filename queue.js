cba90f72afe185d321534bbd71dbabf43359c427
Charlie Harrington <charlie@whatrocks.org>
2017-04-29 14:54:33 -0700

    es6 class queue

--- queue.js
+++ queue.js
@@ -25,10 +25,39 @@ Queue.prototype.peek = function() {
   return this.storage[0];
 }
 
-var myQ = new Queue();
+// var myQ = new Queue();
+// myQ.enqueue(1);
+// myQ.enqueue(2);
+// console.log(myQ);
+// var firstOut = myQ.dequeue();
+// console.log("firstOut: ", firstOut);
+// console.log(myQ);
+
+
+class QueueES6 {
+
+  constructor() {
+    this.storage = [];
+    this.length = 0;
+  }
+
+  enqueue(val) {
+    this.storage.push(val);
+    this.length++;
+  }
+
+  dequeue() {
+    if (!this.length) return;
+    this.length--;
+    return this.storage.shift();
+  }
+}
+
+var myQ = new QueueES6();
 myQ.enqueue(1);
 myQ.enqueue(2);
 console.log(myQ);
 var firstOut = myQ.dequeue();
 console.log("firstOut: ", firstOut);
+myQ.enqueue(4);
 console.log(myQ);
